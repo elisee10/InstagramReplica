9ed9bf8cf375121604cb1317308fad73e18b95e9
Elisee Djapa <eliseedjapa@gmail.com>
2020-10-28 19:47:33 -0400

    Instagram Parser Part 1

--- app/src/main/java/com/example/instagramreplica/MainActivity.java
+++ app/src/main/java/com/example/instagramreplica/MainActivity.java
@@ -20,6 +20,7 @@ import androidx.core.content.FileProvider;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
@@ -68,8 +69,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Description cannot be empty", Toast.LENGTH_LONG).show();
                     return;
                 }
+                if (photoFile == null || ivPostImage.getDrawable() == null){
+                    Toast.makeText(MainActivity.this, "There is no image!", Toast.LENGTH_LONG).show();
+                    return;
+                }
                 ParseUser currentUser  = ParseUser.getCurrentUser();
-                savePost(description, currentUser);
+                savePost(description, currentUser, photoFile);
 
             }
         });
@@ -130,10 +135,10 @@ public class MainActivity extends AppCompatActivity {
         return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
 
-    private void savePost(String description, ParseUser currentUser) {
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
         Post post = new Post();
         post.setDescription(description);
-        //post.setImage()
+        post.setImage(new ParseFile(photoFile));
         post.setUser(currentUser);
         post.saveInBackground(new SaveCallback() {
             @Override
@@ -144,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                  Log.i(TAG, "Post save was successful!!");
                 etDescription.setText("");
+                ivPostImage.setImageResource(0);
 
             }
         });
